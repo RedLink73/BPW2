9771cfae3e3f16b90c9b6b13c53a6c8924ea8139
Joystick <j.de.wekker@gmail.com>
2023-06-16 22:41:54 +0200

    Final Commit

--- Assets/TutorialManager.cs
+++ Assets/TutorialManager.cs
@@ -1,18 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TutorialManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] popUps;
+    private int popUpIndex;
 
-    // Update is called once per frame
+
     void Update()
     {
+        for (int i = 0; i < popUps.Length; i++)
+        {
+            if (i == popUpIndex)
+            {
+                popUps[i].SetActive(true);
+            }
+            else
+            {
+                popUps[i].SetActive(false);
+            }
+        }
+
         
+        if (popUpIndex == 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+
+            {
+                Debug.Log(popUpIndex);
+                popUpIndex++;
+            }
+        }
+
+
+        else if (popUpIndex == 1)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                Debug.Log(popUpIndex);
+
+                popUpIndex++;
+            } 
+        }
+
+
+        else if (popUpIndex == 2)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log(popUpIndex);
+
+                popUpIndex++;
+            }
+        }
+
+
+        else if (popUpIndex == 3)
+        {
+            if (Input.anyKeyDown)
+            {
+                Debug.Log(popUpIndex);
+
+                popUpIndex++;
+            }
+        }
+
+
     }
 }
+
+
+
+
